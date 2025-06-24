a7175f73c71f4b1833cbdd791a7bfa9ff3e3e501
SANSKAR <sanskar32lke@gmail.com>
2025-06-24 17:01:15 +0530

    fail

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 const Loading = () => {
   return (
     <div className="text-center">
-      <img src="loading.gif"></img>
+      <img src="http://localhost:5173/SanskarNews/public/loading.gif"></img>
     </div>
   );
 };
